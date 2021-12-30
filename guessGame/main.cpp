bd62f56a3fbdfef87e42889355ca9d2e357e0663
michalgorny <!2#qweasdZXC>
2021-12-30 12:10:28 +0100

    Initial Commit

---
+++ guessGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  guessGame
+//
+//  Created by Michał GÓRNY on 30/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
