8b80a5f07f4a2576f5d2090b77e734a58be960de
Michał Górny <michal.x.gorny@gmail.com>
2021-12-30 13:45:20 +0100

    Xcode git commit test

--- guessGame/main.cpp
+++ guessGame/main.cpp
@@ -1,14 +1,38 @@
-//
-//  main.cpp
-//  guessGame
-//
-//  Created by Michał GÓRNY on 30/12/2021.
-//
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int x, y, z, a;
+
+int main() {
+    cout << "Hello, World!\n";
+    cout << " Do you wanna play a game with me? \n";
+    cout<< "The rules are simple, You give me two numbers and i draft one between the range \n";
+    cout << "Your job is to guess my choise! \n " ;
+    cout << " Can we start now? pleas choose the first number: \n";
+    cin >> x ;
+    cout << "great! Now please choose another one: \n";
+    cin >> y;
+    
+    cout << " great now can you guess my choise?";
+    
+
+    
+  
+
+    /* do {
+       printf ("Guess the number (1 to 10): ");
+       scanf ("%d",&iGuess);
+       if (iSecret<iGuess) puts ("The secret number is lower");
+       else if (iSecret>iGuess) puts ("The secret number is higher");
+     } while (iSecret!=iGuess);
+
+     puts ("Congratulations!");*/
+    
+    
     return 0;
 }
